e1e2b0f2b62819cf1e751454b92c4ce628391dc8
“Dinuka <it21024818@my.sliit.lk>
2024-05-14 13:48:50 +0530

    new models added

--- DinoAR/DinoAR/ContentView.swift
+++ DinoAR/DinoAR/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import RealityKit
+import AVFoundation
 
 struct ContentView : View {
     var body: some View {
@@ -16,52 +17,70 @@ struct ContentView : View {
 
 struct ARViewContainer: UIViewRepresentable {
     
+    private let backgroundSoundURL = Bundle.main.url(forResource: "background_sound", withExtension: "mp3")!
+    private var backgroundSoundPlayer: AVAudioPlayer?
+    
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
 
-//        // Create a cube model
-//        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-//        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-//        let model = ModelEntity(mesh: mesh, materials: [material])
-//        model.transform.translation.y = 0.05
-//
-//        // Create horizontal plane anchor for the content
-//        let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-//        anchor.children.append(model)
-//
-
-
         // Load and position dinosaurs on a horizontal plane
-        loadAndPositionDinosaur(named: "dinosaur_animation.usdz", at: [0.0, 0.0, -0.5], in: arView)
-        loadAndPositionDinosaur(named: "Pteradactal.usdz", at: [0.2, 0.0, -0.5], in: arView)
-//        loadAndPositionDinosaur(named: "dinosaur3.usdz", at: [-0.2, 0.0, -0.5], in: arView)
+        loadAndPositionDinosaur(named: "dinosaur_animation.usdz", at: [0.0, 0.0, -0.5], scale: SIMD3<Float>(0.005, 0.005, 0.005), in: arView)
+        loadAndPositionDinosaur(named: "Pteradactal.usdz", at: [0.2, 0.5, -0.5], scale: SIMD3<Float>(0.005, 0.005, 0.005), in: arView)
+        loadAndPositionDinosaur(named: "Dinosaur.usdz", at: [-0.2, 0.0, -0.5], scale: SIMD3<Float>(0.005, 0.005, 0.005), in: arView)
+        loadAndPositionDinosaur(named: "oaktrees.usdz", at: [2, 0.0, -0.5], scale: SIMD3<Float>(0.005, 0.005, 0.005), in: arView)
+        loadAndPositionDinosaur(named: "rock.usdz", at: [-1.5, 0.0, -0.5], scale: SIMD3<Float>(0.001, 0.001, 0.001), in: arView)
 
 
+        // Play background sound
+        context.coordinator.playBackgroundSound()
+        
         return arView
         
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
-    private func loadAndPositionDinosaur(named filename: String, at position: SIMD3<Float>, in arView: ARView) {
+    private func loadAndPositionDinosaur(named filename: String, at position: SIMD3<Float>, scale: SIMD3<Float>, in arView: ARView) {
         // Load dinosaur model
         let dinosaur = try! ModelEntity.loadModel(named: filename)
         
         // Position dinosaur on a horizontal plane
         let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.2, 0.2])
         anchor.addChild(dinosaur)
-        dinosaur.setScale(SIMD3<Float>(0.005, 0.005, 0.005), relativeTo: nil)
+        dinosaur.setScale(scale, relativeTo: nil)
         dinosaur.position = position
         
         // Check if the model has animations
-        if let animations = dinosaur.availableAnimations, !animations.isEmpty {
+        if dinosaur.availableAnimations.count > 0 {
             // If animations are available, play the first one
-            dinosaur.playAnimation(animations[0].repeat(duration: .infinity), transitionDuration: 0.25)
+            dinosaur.playAnimation(dinosaur.availableAnimations[0].repeat(duration: .infinity), transitionDuration: 0.25)
         }
         
         // Add anchor to the scene
         arView.scene.addAnchor(anchor)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator {
+        var parent: ARViewContainer
+        
+        init(_ parent: ARViewContainer) {
+            self.parent = parent
+        }
+        
+        func playBackgroundSound() {
+            do {
+                parent.backgroundSoundPlayer = try AVAudioPlayer(contentsOf: parent.backgroundSoundURL)
+                parent.backgroundSoundPlayer?.numberOfLoops = -1 // Infinite loop
+                parent.backgroundSoundPlayer?.play()
+            } catch {
+                print("Error playing background sound: \(error.localizedDescription)")
+            }
+        }
     }
     
 }
